a7c402985c44657504ccd280d7c5b523ac7ee5e7
cafetamine <arek.bystrowicz@gamil.com>
2020-08-01 21:54:22 +0200

    enable eureka server

--- ers-service/src/main/java/com/actilive/ers/ErsApplication.java
+++ ers-service/src/main/java/com/actilive/ers/ErsApplication.java
@@ -2,7 +2,9 @@ package com.actilive.ers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ErsApplication {
 
